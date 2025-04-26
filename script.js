750ae5dd4d0c9e4e041b78572a05ca58cb5d9aee
SAArnol <seinaarnold02@gmail.com>
2025-04-26 08:12:12 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+function updateRates() {
+  document.getElementById("usdRate").innerText = document.getElementById("usdInput").value;
+  document.getElementById("eurRate").innerText = document.getElementById("eurInput").value;
+  document.getElementById("gbpRate").innerText = document.getElementById("gbpInput").value;
+  document.getElementById("kesRate").innerText = document.getElementById("kesInput").value;
+}
